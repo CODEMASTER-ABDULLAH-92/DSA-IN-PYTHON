cc0df51633f39d31fa3f6dd71b98876d40b89f94
CODEMASTER-ABDULLAH-92 <abdullahpeerzada92@gmail.com>
2025-11-18 21:32:24 +0500

    Add linear search implementation in Python
    Added linearSearch.py with a function to perform linear search on a list and print the index if the target is found. Also removed .gitattributes and README.md files.

---
+++ Searching Algorithms/linearSearch.py
@@ -0,0 +1,27 @@
+
+# my_list = [23,54,44,32,65]
+# def linearSearch(target):
+#     for i in my_list:
+#         if(i == target):
+#             print(f"Elment is present on {i}")
+#             return
+#     print("Element is not present")
+
+# linearSearch(32)
+
+
+# for index, value in enumerate(my_list):
+#     print(index, value)
+
+
+my_list = [23,54,44,32,65]
+def linearSearch(target):
+    for idx, value in enumerate(my_list):
+        if(value == target):
+            print(f"Elment is present on {idx}")
+            return
+    print("Element is not present")
+
+linearSearch(32)
+
+
